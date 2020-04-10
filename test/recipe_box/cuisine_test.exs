fdc6185c6840055a1cedea41161c4a093aa82e41
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-04-10 17:12:22 -0500

    add test for duplicate username validation

--- test/recipe_box/cuisine_test.exs
+++ test/recipe_box/cuisine_test.exs
@@ -10,10 +10,15 @@ defmodule RecipeBox.CuisineTest do
 
     test "with valid data inserts cuisine" do
       assert {:ok, %Cuisine{id: id}=cuisine} = Cuisine.create_cuisine(@valid_attrs)
-      assert cuisine.name == "Italian"
+      assert cuisine.name == "italian"
     end
 
     test "does not insert duplicate cuisine" do
+      assert {:ok, %Cuisine{id: id}} = Cuisine.create_cuisine(@valid_attrs)
+
+      assert {:error, changeset} = Cuisine.create_cuisine(@valid_attrs)
+
+      assert errors_on(changeset) == %{name: ["has already been taken"]}
     end
   end
 end
